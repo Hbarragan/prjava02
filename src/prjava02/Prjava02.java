4d9a5a5e661df6d5daa0b4fcddc9e87cd7e2e0a3
Hector_2 <Hector_2@192.168.1.36>
2014-04-27 12:24:18 +0200

    Versió 0.1 del projecte prjava01

---
+++ src/prjava02/Prjava02.java
@@ -0,0 +1,42 @@
+
+/*
+
+To change this template, choose Tools | Templates 
+
+and open the template in the editor. 
+
+*/
+
+package prjava02;
+import java.io.*; 
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+
+*
+
+* @author dc1314daw@gmail.com */
+
+public class Prjava02 {
+
+/**
+
+* @param args the command line arguments */
+
+public static void main(String[] args) { 
+
+    InetAddress addr = null; 
+    try {
+        addr = InetAddress.getLocalHost();
+    } catch (UnknownHostException ex) {
+        Logger.getLogger(Prjava02.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    //byte[] ipAddr = addr.getAddress();
+    String hostname = addr.getHostName(); System.out.println("hostname="+hostname);
+    System.out.println("Nom de l'usuari: " + System.getProperty("user.name")); System.out.println("Carpeta Personal: " + System.getProperty("user.home")); System.out.println("Sistema operatiu: " + System.getProperty("os.name")); System.out.println("Versió OS: " + System.getProperty("os.version"));
+
+
+}
+}
